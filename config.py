d7483f30511e8b95ae26bb53108c0fd0abec0caf
knows RDX <54053473+knownrdx@users.noreply.github.com>
2025-07-15 11:14:11 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,113 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "2006456745:AAEkaj5ZyFzL6DTGtXkuqltLbgSXOPpJKlY"
+OWNER_ID = 789019025
+TELEGRAM_API = 8569532
+TELEGRAM_HASH = "4af251c2934e0eaebddeab384c829507"
+# OPTIONAL CONFIG
+TG_PROXY = {}
+LOG_CHANNEL = "-1002643508706"
+DOWNLOAD_DIR= "downloads"
+USER_SESSION_STRING = "BQCCwrwAtqAG99omqj_Jw7i4nVoV5e9VNmTNsxwYGA0iu9wqa17s1JrKDjG8Qa6qm8tpdEeIG6sTdpiHUGCdXeP0i1q-rSvb_t615YXb2PL-E3fqt_ZxHG4knGV1vWCsQmMTG-_BHpG6lfKxJeRIrQkfXS-fZqiBNA1VYXuNIcha6LSEpGYsd8np3enAhO29gGXY9VSuDY9ier_NFqCb47th-z78MQxE4r-KYA-NLoj4-cf_VVwB4PsEegOfoq3QO1BorQmrjoEvGXyjEjLODwRg93y-btSm1qKjGlV6m5jQ8u0NSJ_qF-4SvYQvMpy6Un1KVyuU9j2OtFDOtFdyC9vjV5Z2dwAAAAAvB3mRAA"
+CMD_SUFFIX = ""
+AUTHORIZED_CHATS = "-1002643508706"
+SUDO_USERS = "789019025"
+DATABASE_URL = "mongodb+srv://rdsislam21:xMiMZA0yxBtk08N5@cluster0.p0zpxef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+STATUS_LIMIT = 6
+DEFAULT_UPLOAD = "tg"
+STATUS_UPDATE_INTERVAL = 15
+FILELION_API = ""
+STREAMWISH_API = ""
+EXCLUDED_EXTENSIONS = ""
+INCOMPLETE_TASK_NOTIFIER = False
+YT_DLP_OPTIONS = ""
+USE_SERVICE_ACCOUNTS = False
+NAME_SUBSTITUTE = ""
+FFMPEG_CMDS = {}
+UPLOAD_PATHS = {}
+# GDrive Tools
+GDRIVE_ID = ""
+IS_TEAM_DRIVE = False
+STOP_DUPLICATE = False
+INDEX_URL = ""
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = 0
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+# JDownloader
+JD_EMAIL = ""
+JD_PASS = ""
+# Sabnzbd
+USENET_SERVERS = [
+    {
+        "name": "main",
+        "host": "",
+        "port": 563,
+        "timeout": 60,
+        "username": "",
+        "password": "",
+        "connections": 8,
+        "ssl": 1,
+        "ssl_verify": 2,
+        "ssl_ciphers": "",
+        "enable": 1,
+        "required": 0,
+        "optional": 0,
+        "retention": 0,
+        "send_group": 0,
+        "priority": 0,
+    }
+]
+# Nzb search
+HYDRA_IP = ""
+HYDRA_API_KEY = ""
+# Update
+UPSTREAM_REPO = ""
+UPSTREAM_BRANCH = "master"
+# Leech
+LEECH_SPLIT_SIZE = 0
+AS_DOCUMENT = False
+EQUAL_SPLITS = False
+MEDIA_GROUP = False
+USER_TRANSMISSION = True
+HYBRID_LEECH = False
+LEECH_FILENAME_PREFIX = ""
+LEECH_DUMP_CHAT = ""
+THUMBNAIL_LAYOUT = ""
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = 0
+BASE_URL = ""
+BASE_URL_PORT = 0
+WEB_PINCODE = False
+# Queueing system
+QUEUE_ALL = 0
+QUEUE_DOWNLOAD = 0
+QUEUE_UPLOAD = 0
+# RSS
+RSS_DELAY = 600
+RSS_CHAT = "-1002643508706"
+RSS_SIZE_LIMIT = 0
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = 0
+SEARCH_PLUGINS = [
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+    "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+    "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+    "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+    "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
+    "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+    "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+    "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py",
+]
